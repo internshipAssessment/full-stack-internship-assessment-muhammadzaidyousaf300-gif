53fa54370b74b5deb531a1629f090c271f08999f
muhammadzaidyousaf300-gif <muhammadzaidyousaf300@gmail.com>
2025-09-10 04:18:33 -0700

    app.js

--- node/app.js
+++ node/app.js
@@ -2,6 +2,17 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-// TODO: implement POST /api/sum
+app.post('/api/sum', (req, res) => {
+    const { a, b } = req.body;
+
+    // Validate input
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return res.status(400).json({ error: 'Invalid input' });
+    }
+
+    // Compute sum
+    const sum = a + b;
+    res.json({ sum });
+});
 
 module.exports = app;
